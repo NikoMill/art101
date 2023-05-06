79a834f1032e7ed81fa631f9253e14d7c54e4f42
NikoMill <amille19@ucsc.edu>
2023-05-06 16:23:53 -0700

    Lab 8!

---
+++ Lab8/lab.js
@@ -0,0 +1,31 @@
+// index.js - Working with your partner, experiment with anonymous functions and callbacks.
+// Author: Niko Miller
+// Date: May 9, 2023
+
+// Constants
+
+// Functions
+
+function isEven(x) {
+    return (x % 2 == 0 );
+}
+
+// test function
+console.log("Is 1 even? ", isEven(1));
+console.log("Is 2 even? ", isEven(2));
+
+Array = [100, 734289, 27, 66, 8, 29]
+console.log("My array", array);
+
+var result = array.map(isEven);
+// should return [true, false, false, true, true, false]
+console.log("Test evenness of array", result);
+
+var result = array.map(function(x){
+    return x ** 0.5;
+})
+// should return [10, 367144.5, 13.5, 33, 4, 14.5]
+console.log("squareroot of array:", result);
+
+
+  
