bd4e92f1b5fd9e99493230b82d404e84e59ed6af
NikoMill <amille19@ucsc.edu>
2023-05-10 16:34:34 -0700

    Lab9

---
+++ Lab9/lab9.js
@@ -0,0 +1,38 @@
+// index.js - Lab 9
+// Author: Niko Miller
+// Date: May 11, 2023
+
+// Constants
+var outputEl = document.getElementById("output");
+
+var new1El = document.getElementById("p");
+new1El.innerHTML("Wassup art 101!!!");
+
+var new2El= document.createElement("p");
+new2El.innerHTML("My name is Niko")
+
+outputEl.appendChild(new1El)
+<div>
+
+
+var
+// Functions
+
+
+
+
+
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+    // some code here
+    // return results;
+  }
+  
+  function main() {
+    console.log("Main function started.");
+    // the code that makes everything happen
+  }
+  
+  // let's get this party started
+  main();
