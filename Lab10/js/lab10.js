d8aa10b5ea6797ecff8e88b9d967e139064f6269
NikoMill <amille19@ucsc.edu>
2023-05-15 20:52:22 -0700

    lab1000000

--- Lab10/js/lab10.js
+++ Lab10/js/lab10.js
@@ -11,12 +11,12 @@ var button = document.getElementById( 'my-button');
  button.addEventListener ('click', function() {
     var inputValue = document.getElementById('my-input').value;
     var inputSplit = inputValue.split('');
-    var sorted = inputSplit.sort('');
+    var sorted = inputSplit.sort();
     var newEl = document.createElement("p");
     newEl.innerHTML = sorted.join('');
     document.getElementById("output").appendChild(newEl);
    }
- ) 
+ );
 
 
 
