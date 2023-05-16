cef50e5c5ccc15d97e00b9cc4324bad938aad235
NikoMill <amille19@ucsc.edu>
2023-05-15 20:42:06 -0700

    lab10...

--- Lab10/js/lab10.js
+++ Lab10/js/lab10.js
@@ -9,30 +9,22 @@
 //of their name
 var button = document.getElementById( 'my-button');
  button.addEventListener ('click', function() {
-    inputValue = document.getElementById('my-input').value;
-    console.log("you input: ", inputValue);}
+    var inputValue = document.getElementById('my-input').value;
+   }
  ) 
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function sortUserName() {
-    var userName = window.prompt ("Hi. Please tell me your name so I can fix it.");
-    //console.log("userName =", userName);
-    //split string to array
-    var nameArray = userName.split('');
-    //console.log("nameArray =", nameArray);
-    // sort the array
-    var nameArraySort = nameArray.sort();
-    //console.log("nameArraySort =", nameArraySort);
-    //join array back to string
-    var nameSorted = nameArraySort.join('');
-   // console.log("nameSorted =", nameSorted);
-   // Note that I could have done the above lines as a single line:
-   // userName.toLower().split("").sort().join("")
-   return nameSorted;
- }
+
+ var inputSplit = inputValue.split('');
+ var sorted = inputSplit.sort('');
+ var nameSorted = sorted.join('');
+ var newEl = document.createElement("p");
+ newEl.innerHTML = sorted.join('');
+ document.getElementById("output").appendChild(newEl);
+
+
 
  
-s
+
  
  
 
