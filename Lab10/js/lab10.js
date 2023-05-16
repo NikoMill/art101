2078be76c3342f4246061a6cc48c082647fff868
NikoMill <amille19@ucsc.edu>
2023-05-15 20:44:05 -0700

    LAB 10

--- Lab10/js/lab10.js
+++ Lab10/js/lab10.js
@@ -10,28 +10,20 @@
 var button = document.getElementById( 'my-button');
  button.addEventListener ('click', function() {
     var inputValue = document.getElementById('my-input').value;
+    var inputSplit = inputValue.split('');
+    var sorted = inputSplit.sort('');
+    var newEl = document.createElement("p");
+    newEl.innerHTML = sorted.join('');
+    document.getElementById("output").appendChild(newEl);
    }
  ) 
 
 
- var inputSplit = inputValue.split('');
- var sorted = inputSplit.sort('');
- var nameSorted = sorted.join('');
- var newEl = document.createElement("p");
- newEl.innerHTML = sorted.join('');
- document.getElementById("output").appendChild(newEl);
-
-
 
  
 
  
  
 
-input 
 
- 
- // output
- document.writeln ("Oh hey, I've fixed your name: ",
-     sortUserName(), "</br>");
   
