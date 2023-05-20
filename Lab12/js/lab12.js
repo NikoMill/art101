3774f1309760ff4604c162725f3fd9613e40a00a
NikoMill <amille19@ucsc.edu>
2023-05-20 13:09:38 -0700

    lab12.

--- Lab12/js/lab12.js
+++ Lab12/js/lab12.js
@@ -5,49 +5,50 @@
 // Constants
 
 // Functions
+var sign= sortingHat (varsign);
 function sortingHat(str) {
     len = str.length;
-    mod = len % 13; 
-    if (mod==0) {
+    mod = len % 12; 
+    if (mod == 0) {
         return "Aries"
     }
-    else if (mod==1){
+    else if (mod == 1) {
         return "Libra"
     }
 
-    else if (mod==3){
+    else if (mod == 3 ){
     return "Taurus"
     }
-    else if (mod==4){
+    else if (mod == 4) {
         return "Pisces"
      }
-    else if (mod==5){
+    else if (mod == 5) {
       return "Leo"
      }
-     else if (mod==6){
+     else if (mod == 6) {
         return "Capricorn"
      }
-    else if (mod==7){
-        return "Sgittarius"
+    else if (mod == 7) {
+        return "Sagittarius"
         }
-    else if (mod==8){
+    else if (mod == 8) {
          return "Gemini"
          }
-    else if (mod==9){
+    else if (mod == 9) {
         return "Cancer"
             }
-    else if (mod==10){
+    else if (mod == 10) {
         return "Aquarius"
                 }
-     else if (mod==11){
+     else if (mod == 11) {
         return "Virgo"
          }
-    else if (mod==12){
+    else if (mod == 2) {
          return "Scorpio"
             }
 }
 
-var myButton = document.getElementById.ById("button");
+var myButton = document.getElementById ("button");
 myButton.addEventListener("click", function (){
     var name = document.getElementById("input").value;
     newText = "<p> I Think you are a..." 
