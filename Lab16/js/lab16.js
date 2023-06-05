31a3bef72fc36904148c4e8d6e70f143c9ca405c
NikoMill <amille19@ucsc.edu>
2023-06-04 17:23:00 -0700

    Update lab16.js

--- Lab16/js/lab16.js
+++ Lab16/js/lab16.js
@@ -4,35 +4,17 @@
 
 // Using the core $.ajax() method
 $.ajax({
-    // The URL for the request (from the api docs)
-    url: "https://xkcd.com/614/info.0.json"
-    // The data to send (will be converted to a query string)
-        ,data:{ 
-          
-          },
-    // Whether this is a POST or GET request
-    type: "GET",
-    // The type of data we expect back
-    dataType : "json",
-    // What do we do when the api call is successful
-    //   all the action goes in here
-    success: function(data) {
-        // do stuff
-        console.log(data); 
-        console.log (data.month);
-        console.log (data.num);
-        console.log (data.link);
-        console.log (data.year);
-        console.log (data.news);
-        console.log (data.safe_title);
-        console.log (data.transcript);
-
-
-
-    },
-    // What we do if the api call fails
-    error: function (jqXHR, textStatus, errorThrown) { 
-        // do stuff
-        console.log("Error:", textStatus, errorThrown);
-    }
+    // URL
+    url: "https://xkcd.com/info.0.json",  
+    // data to be sent
+    data: {},
+    type: "GET"
 })
+.done(function(comicObj){
+    $("#output").html("<h2> " + comicObj.title);
+    $("#output").append("<img src='" + comicObj.img + "'/>");
+    $("#output").append("<p>alt: " + comicObj.alt );
+    })
+.fail(function(jqXHR, textStatus, errorThrown){
+    console.log("Error: ", textStatus, errorThrown);
+    })
